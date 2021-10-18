715ac663a24a1739fa1a66f8816a2074b3e14101
Pavel Dimens <pdimens@live.com>
2021-10-18 15:01:49 -0400

    update module docstring

--- src/PopGenSims.jl
+++ src/PopGenSims.jl
@@ -1,3 +1,25 @@
+"""
+# Population genetics simulations
+Repository:    https://www.github.com/pdimens/PopGenSims.jl/
+Documentation: https://biojulia.net/PopGen.jl/
+
+\nA few things things you can do to get started:
+
+## Import Data using PopGenCore
+- `PopGenCore.read(filename; kwargs...)`
+- `genepop(infile; kwargs...)`  or similar file-specific importer
+- use available `@gulfsharks` or `@nancycats` datasets
+
+## Simulate Samples within Populations
+- `simulate(popdata, n = 100)` to simulate samples using population-specific allele frequencies
+
+## Simulate breeding crosses
+- `cross(popdata, parent1, parent2, ...)` to cross two parents from the same PopData
+- `cross(popdata => parent1, popdata => parent2, ...) to cross two parents from different PopData (e.g. backcross) 
+
+## Simulate siblingship
+- `simulate_sibship(popdata, fullsib = , halfsib = , unrelated = , ...)` to simulate breeding events generating pairs of individuals of known kinship.
+"""
 module PopGenSims
 
 using DataFrames, PooledArrays, StaticArrays
