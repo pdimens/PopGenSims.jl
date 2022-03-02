c0555c99a4152fe2e7ea2e22f6338e68d3aced1f
Pavel Dimens <pdimens@live.com>
2022-03-02 11:44:05 -0500

    add proportional sample method

--- src/Samples.jl
+++ src/Samples.jl
@@ -30,58 +30,41 @@ function sample_locus(locus::Dict, n::Int, ploidy::Signed)
 end
 
 """
-    simulate(data::PopData; n::Int = 100)
-Simulate `n` number of individuals per population (default: `100`) using per-population
-allele frequencies derived from a `PopData` object. Returns a new `PopData` object.
+    simulate(data::PopData; n::Int)
+Simulate `n` number of individuals per population using per-population
+allele frequencies derived from a `PopData` object. Returns a new `PopData` object with `n` * `n_populations` samples.
+
+    simulate(data::PopData; scale::Int)
+Simulate individuals per population in the same proportions they appear in the PopData
+using per-population allele frequencies. Simulation volume can be multiplied using `scale`,
+i.e. if you want to keep the same proportions but generate twice the number of samples, `scale`
+would be `2`. Returns a new `PopData` object with `n_samples` * `scale` samples.    
 
 **Example**
 ```julia
-cats = @nanycats;
+julia> cats = @nanycats;
 
-julia> sims = simulate(cats , n = 100)
+julia> sims = simulate(cats, n = 100)
 PopData{Diploid, 9 Microsatellite Loci}
   Samples: 1700
   Populations: 17
   
-julia> sims.sampleinfo
-
-  1700×5 DataFrame
-  Row │ name      population  ploidy   
-      │ String    String      Int8      
-──────┼───────────────────────────────
-    1 │ sim_1     1                2    
-    2 │ sim_2     1                2    
-    3 │ sim_3     1                2    
-    4 │ sim_4     1                2    
-    5 │ sim_5     1                2    
-  ⋮   │    ⋮          ⋮         ⋮ 
- 1697 │ sim_1697  17               2  
- 1698 │ sim_1698  17               2  
- 1699 │ sim_1699  17               2  
- 1700 │ sim_1700  17               2  
-                                         1691 rows omitted
-
-julia> sims.genodata
-15300×4 DataFrame
-   Row │ name      population  locus   genotype   
-       │ String    String      String  Tuple…?    
-───────┼──────────────────────────────────────────
-     1 │ sim_1     1           fca8    (135, 143)
-     2 │ sim_1     1           fca23   (136, 146)
-     3 │ sim_1     1           fca43   (141, 145)
-     4 │ sim_1     1           fca45   (120, 126)
-     5 │ sim_1     1           fca77   (156, 156)
-   ⋮   │    ⋮          ⋮         ⋮         ⋮
- 15297 │ sim_1700  17          fca78   (150, 150)
- 15298 │ sim_1700  17          fca90   (197, 197)
- 15299 │ sim_1700  17          fca96   (113, 113)
- 15300 │ sim_1700  17          fca37   (208, 208)
-                                15291 rows omitted
+julia> sims_prop = simulate(cats, scale = 3)
+  PopData{Diploid, 9 Microsatellite Loci}
+    Samples: 711
+    Populations: 17
 ```
 """
-function simulate(data::PopData; n::Int = 100)
-    data.metadata.ploidy != 2 && error("Simulations do not work on mixed-ploidy data (yet)")
+function simulate(data::PopData; n::Int=0, scale::Int = 0)
+    n == scale == 0 && throw(ArgumentError("Please use one of n (flat) or scale (proportional) keywords for simulations. See ?simulate for more info."))
+    !iszero(n) & !iszero(scale) && throw(ArgumentError("Must use only one of n (flat) or scale (proportional) keywords for simulations. See ?simulate for more info."))
+    !iszero(n) & iszero(scale) && return _simulateflat(data, n)
+    iszero(n) & !iszero(scale) && return _simulatescale(data, scale)
+end
+
+function _simulateflat(data::PopData, n::Int)
     ploidy = data.metadata.ploidy
+    length(ploidy) > 1 && error("Simulations do not work on mixed-ploidy data (yet)")
     pops = unique(data.sampleinfo.population)
     npops = data.metadata.populations
     nloci = data.metadata.loci
@@ -101,6 +84,42 @@ function simulate(data::PopData; n::Int = 100)
     # create new genotypes
     transform!(freqs, :frq => (i -> sample_locus.(i,n,ploidy)) => :frq)
     
+    # populate out df
+    out_gdf = groupby(geno_out, :population)
+    geno_gdf = groupby(freqs, :population)
+    for pop in pops
+        out_gdf[(population = pop,)][:,:genotype] = reduce(hcat, geno_gdf[(population = pop,)].frq) |> permutedims |> vec
+    end
+    transform!(
+        geno_out,
+        :name => (i -> PooledArray(i, compress = true)) => :name,
+        :population => (i -> PooledArray(i, compress = true)) => :population,
+        :locus => (i -> PooledArray(i, compress = true)) => :locus,
+        :genotype
+    )
+    PopData(geno_out)
+end
+
+function _simulatescale(data::PopData, n::Int)
+    ploidy = data.metadata.ploidy
+    length(ploidy) > 1 && error("Simulations do not work on mixed-ploidy data (yet)")
+    pops = unique(data.sampleinfo.population)
+    popcounts =  [count(i -> i == j, data.sampleinfo.population) for j in pops]
+    nloci = data.metadata.loci
+
+    # instantiate output df
+    simnames = repeat(["sim_" * "$i" for i in 1:(data.metadata.samples * n)], inner = nloci)
+    popnames = reduce(vcat, [fill(i, nloci * j * n) for (i,j) in zip(pops,popcounts)])
+    locinames = repeat(unique(data.genodata.locus), outer = (n * data.metadata.samples))
+    geno_out = DataFrame(:name => simnames, :population => popnames, :locus => locinames, :genotype => similar(data.genodata.genotype, length(locinames)))
+    # generate allele freqs per population
+    gdf = groupby(data.genodata, [:population, :locus])
+    freqs = DataFrames.combine(
+        gdf,
+        :genotype => allelefreq => :frq
+    )
+    freqs[:, :n] .= repeat(popcounts, inner = nloci)    
+    transform!(freqs, [:frq, :n] => ((i,j) -> sample_locus.(i, j*n, ploidy)) => :frq)
     # populate out df
     out_gdf = groupby(geno_out, :population)
     geno_gdf = groupby(freqs, :population)
